26f88d89944181576a9a5499e47ac5365af3cf0c
fernando.js8 <fernando.js8@outlook.com>
2018-12-09 21:03:35 -0200

    First Commit

---
+++ 06 Strings Expressoes Regular e a Classe Object/ByteBank.SistemaAgencia/ExtratorValorArgumentosURL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorValorArgumentosURL
+    {
+        public string URL { get; set; }
+        private readonly string _argumentos = string.Empty;
+
+        public ExtratorValorArgumentosURL(string url)
+        {
+            if (String.IsNullOrWhiteSpace(url))
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio.", nameof(url));
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+
+            URL = url;
+        }
+
+        public string GetValor(string nomeParametro)
+        {
+            nomeParametro = nomeParametro.ToUpper();
+            string argumentosEmCaixaAlta = _argumentos.ToUpper();
+            string termo = nomeParametro + "=";
+            int indiceTermo = argumentosEmCaixaAlta.IndexOf(termo);
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if (indiceEComercial == -1)
+                return resultado;
+
+            return resultado.Remove(indiceEComercial);
+        }
+    }
+}
---
+++ 06 Strings Expressoes Regular e a Classe Object/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urlParametros = "http://www.bytebank.com/pagina?moedaOrigem=Real&moedaDestino=Dollar";
+            ExtratorValorArgumentosURL extratorValor = new ExtratorValorArgumentosURL(urlParametros);
+            string valor = extratorValor.GetValor("MOEDADESTINO");
+
+            if (valor.ToUpper().Contains("DOLLAR"))
+                Console.WriteLine(valor.ToUpper().StartsWith("DOL") && valor.ToUpper().EndsWith("LAR"));
+
+            Console.WriteLine("Valor de Moeda Destino = " + valor);
+
+            Console.Read();
+
+
+
+            bool testarHumanizer = false;
+            if (testarHumanizer)
+                UtilizandoHumanizer();
+        }
+
+        private static void UtilizandoHumanizer()
+        {
+            DateTime dataFimPagamento = new DateTime(2018, 6, 20);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(40); //dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+        }
+    }
+}
