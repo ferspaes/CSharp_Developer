c98f8c40975649a2dfb1de16b8a49256bacb58cf
fernando.js8 <fernando.js8@outlook.com>
2018-12-12 13:26:49 -0200

    Course 07 begin, first commit.

--- 07 Arrays e Tipos Genericos/ByteBank.SistemaAgencia/Program.cs
+++ 07 Arrays e Tipos Genericos/ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,17 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            int[] idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 30;
+            idades[2] = 45;
+            idades[3] = 60;
+            idades[4] = 75;
+
+            foreach (var idade in idades)
+                Console.WriteLine(idade);
+
             Console.ReadLine();
         }
     }
