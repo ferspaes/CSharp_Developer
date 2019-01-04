f4f5c4e8e66d2d6cbfe05ed8cd098158d9a72d5e
fernando.js8 <fernando.js8@outlook.com>
2019-01-03 22:02:50 -0200

    Aula 09 20%! OK!

--- 09_IOStreams/ByteBankImportacaoExportacao/Program.cs
+++ 09_IOStreams/ByteBankImportacaoExportacao/Program.cs
@@ -1,18 +1,43 @@
 ﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ByteBankImportacaoExportacao 
-{ 
-    class Program 
-    { 
-        static void Main(string[] args) 
+namespace ByteBankImportacaoExportacao
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
+            string pathTXT = "contas.txt";
+
+            var buffer = new byte[1024];
+
+            var fluxoArquivo = new FileStream(pathTXT, FileMode.Open); // FileStream(pathTXT, FileMode)3);
+
+            int contadorLeitura = -1;
+
+            while (contadorLeitura != 0)
+            {
+                contadorLeitura = fluxoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+                Console.WriteLine();
+                Console.WriteLine("Leu!!!");
+            }
+
             Console.ReadLine();
         }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write("");
+            }
+        }
     }
-} 
- 
+}
