c305b9bbcde2bf7cf5684d2e05be4aff6563b466
fernando.js8 <fernando.js8@outlook.com>
2019-01-07 23:23:43 -0200

    Aula02 Concluida Com Sucesso 100%! OK! :D

---
+++ 09_IOStreams/ByteBankImportacaoExportacao/1_ManipuladorFileStream.cs
@@ -0,0 +1,50 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        void ManipuladorFileStream()
+        {
+            string pathExercicio = "teste.txt";
+            string pathTXT = "contas.txt";
+            var buffer = new byte[1024];
+            var fluxoArquivo = new FileStream(pathTXT, FileMode.Open); // FileStream(pathTXT, FileMode)3);
+            var encoding = Encoding.Default;
+
+            using (var fsExercicio = new FileStream(pathExercicio, FileMode.Open))
+            {
+                int contadorLeitura = -1;
+
+                while (contadorLeitura != 0)
+                {
+                    contadorLeitura = fsExercicio.Read(buffer, 0, buffer.Length);
+                    string conteudoArquivo = encoding.GetString(buffer, 0, contadorLeitura);
+
+                    if (!string.IsNullOrWhiteSpace(conteudoArquivo))
+                    {
+                        Console.WriteLine(conteudoArquivo);
+                        Console.WriteLine("Leu!!!");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write("");
+            }
+        }
+    }
+}
--- 09_IOStreams/ByteBankImportacaoExportacao/Program.cs
+++ 09_IOStreams/ByteBankImportacaoExportacao/Program.cs
@@ -8,48 +8,22 @@ using System.Threading.Tasks;
 
 namespace ByteBankImportacaoExportacao
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            string pathExercicio = "teste.txt";
-            string pathTXT = "contas.txt";
-            var buffer = new byte[1024];
-            var fluxoArquivo = new FileStream(pathTXT, FileMode.Open); // FileStream(pathTXT, FileMode)3);
-            var fsExercicio = new FileStream(pathExercicio, FileMode.Open);
-            var listFileStream = new List<FileStream>();
-            var encoding = Encoding.Default;
-
-            listFileStream.Add(fluxoArquivo);
-            listFileStream.Add(fsExercicio);
-
-            foreach (var arquivo in listFileStream)
+            string pathArquivo = "contas.txt";
+            using (var fs = new FileStream(pathArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fs))
             {
-                int contadorLeitura = -1;
-
-                while (contadorLeitura != 0)
+                while (!leitor.EndOfStream)
                 {
-                    contadorLeitura = arquivo.Read(buffer, 0, buffer.Length);
-                    var conteudoArquivo = encoding.GetString(buffer, 0, contadorLeitura);
-
-                    if (!string.IsNullOrWhiteSpace(conteudoArquivo))
-                    {
-                        Console.WriteLine(conteudoArquivo);
-                        Console.WriteLine("Leu!!!");
-                    }
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
                 }
             }
 
             Console.ReadLine();
-        }
-
-        static void EscreverBuffer(byte[] buffer)
-        {
-            foreach (var meuByte in buffer)
-            {
-                Console.Write(meuByte);
-                Console.Write("");
-            }
         }
     }
 }
