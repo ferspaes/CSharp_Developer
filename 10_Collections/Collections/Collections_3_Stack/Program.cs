30eeca81f3c1dc497c27d4caed3da035b908858c
fernando.js8 <fernando.js8@outlook.com>
2019-01-13 10:40:33 -0200

    Curso - C# Collections - Progress = 73%, OK, Success!

---
+++ 10_Collections/Collections/Collections_3_Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using Collections_3_Stack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_3_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chrome = new Navegador();
+
+            chrome.Navegar("www.google.com.br");
+            chrome.Navegar("www.facebook.com.br");
+            chrome.Navegar("www.outlook.com");
+
+            chrome.Voltar();
+
+            chrome.Avancar();
+
+            chrome.Voltar();
+            chrome.Voltar();
+            chrome.Voltar();
+            chrome.Voltar();
+
+            chrome.Avancar();
+            chrome.Avancar();
+            chrome.Avancar();
+            chrome.Avancar();
+
+            chrome.VerHistorico();
+
+            Console.Read();
+        }
+    }
+}
