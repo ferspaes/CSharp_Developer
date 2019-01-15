18a4abae15d6cb080eacf639a8ff57c82fa7cff7
fernando.js8 <fernando.js8@outlook.com>
2019-01-15 13:19:42 -0200

    Curso - C# Collections 2 - Progress = 26% - Success, OK!

--- 11_Collections_2/Collections_2/Collections_2/Program.cs
+++ 11_Collections_2/Collections_2/Collections_2/Program.cs
@@ -11,7 +11,7 @@ namespace Collections_2
     {
         static void Main(string[] args)
         {
-            IDictionary<string, Aluno> alunos = new Dictionary<string, Aluno>();
+            IDictionary<string, Aluno> alunos = new SortedDictionary<string, Aluno>();
             alunos.Add("AK", new Aluno("Amanda", 4738942));
             alunos.Add("GL", new Aluno("Guilherme", 5437389));
             alunos.Add("AP", new Aluno("Ana", 34783128));
@@ -41,7 +41,41 @@ namespace Collections_2
                 Console.WriteLine(aluno);
             }
 
+
+            ISet<string> colecao = new SortedSet<string>(new Comparador());
+
+            ISet<string> outroConjunto = new HashSet<string>();
+
+            //este conjunto é subconjunto de outro? IsSubsetOf
+            colecao.IsSubsetOf(outroConjunto);
+
+            //este conjunto é superconjunto de outro? IsSupersetOf
+            colecao.IsSupersetOf(outroConjunto);
+
+            //os conjuntos contêm os mesmo elementos? SetEquals
+            colecao.SetEquals(outroConjunto);
+
+            //subtrai os elementos da outra coleção que também estão nesse? ExceptWith
+            colecao.ExceptWith(outroConjunto);
+
+            //intersecção dos conjuntos - IntersectWith
+            colecao.IntersectWith(outroConjunto);
+
+            //somente em um ou outro conjunto - SymmetricExceptWith
+            colecao.SymmetricExceptWith(outroConjunto;
+
+            //união de conjuntos - UnionWith
+            colecao.UnionWith(outroConjunto);
+
             Console.Read();
+        }
+    }
+
+    internal class Comparador : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
