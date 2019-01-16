2af45039992dac76289cdb2abb8274b6cd0ebea8
fernando.js8 <fernando.js8@outlook.com>
2019-01-15 23:55:52 -0200

    Curso: C# Collections - Progress: 59%, OK! Success!

---
+++ 11_Collections_2/Collections_2/Collections_2B_LINQ/Model/Mes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_2B_LINQ.Model
+{
+    public class Mes
+    {
+        private string nome;
+        private int dias;
+
+        public Mes(string nome, int dias)
+        {
+            this.nome = nome;
+            this.dias = dias;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Dias
+        {
+            get { return dias; }
+            set { dias = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome.PadRight(10)} - {Dias} ";
+        }
+    }
+}
---
+++ 11_Collections_2/Collections_2/Collections_2B_LINQ/Program.cs
@@ -0,0 +1,47 @@
+﻿using Collections_2B_LINQ.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_2B_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mes> meses = new List<Mes>
+            {
+                new Mes("Janeiro", 31),
+                new Mes("Fevereiro", 28),
+                new Mes("Março", 31),
+                new Mes("Abril", 30),
+                new Mes("Maio", 31),
+                new Mes("Junho", 30),
+                new Mes("Julho", 31),
+                new Mes("Agosto", 31),
+                new Mes("Setembro", 30),
+                new Mes("Outubro", 31),
+                new Mes("Novembro", 30),
+                new Mes("Dezembro", 31)
+            };
+
+            meses.ForEach(mes => Console.WriteLine(mes));
+            Console.WriteLine();
+
+            IEnumerable<string> query =
+                meses
+                .Where(mes => mes.Dias.Equals(31))
+                .OrderBy(mes => mes.Nome)
+                .Select(mes => mes.Nome.ToUpper());
+
+            foreach (var mes in query)
+            {
+                Console.WriteLine(mes);
+            }
+
+            Console.Read();
+        }
+    }
+}
