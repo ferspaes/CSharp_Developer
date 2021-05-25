1a785578b15e45a2d0ca3624a11b6b165a836ab9
fernando.js8 <fernando.js8@outlook.com>
2021-05-25 18:59:11 -0300

    Clean Update

--- 11_Collections_2/Collections_2/Collections_2B_LINQ/Program.cs
+++ 11_Collections_2/Collections_2/Collections_2B_LINQ/Program.cs
@@ -38,9 +38,7 @@ namespace Collections_2B_LINQ
 
             Console.WriteLine($"\n(\nmeses\n.Where(mes => mes.Dias.Equals(31))\n.OrderBy(mes => mes.Nome)\n.Select(mes => mes.Nome.ToUpper());\n)\n");
             foreach (var mes in query)
-            {
                 Console.WriteLine(mes);
-            }
 
             // Pega os 3 primeiros meses da lista
             var consulta1 = meses.Take(3);
@@ -60,107 +58,76 @@ namespace Collections_2B_LINQ
             Console.WriteLine($"\nTake(3)\n");
 
             foreach (var mes in consulta1)
-            {
                 Console.WriteLine(mes);
-            }
 
             Console.WriteLine($"\nSkip(3)\n");
 
             foreach (var mes in consulta2)
-            {
                 Console.WriteLine(mes);
-            }
 
             Console.WriteLine($"\nSkip(6).Take(3)\n");
 
             foreach (var mes in consulta3)
-            {
                 Console.WriteLine(mes);
-            }
 
             Console.WriteLine($"\nTakeWhile(mes => !mes.Nome.StartsWith('S'))\n");
 
             foreach (var mes in consulta4)
-            {
                 Console.WriteLine(mes);
-            }
 
             Console.WriteLine($"\nSkipWhile(mes => !mes.Nome.StartsWith('S'))\n");
 
             foreach (var mes in consulta5)
-            {
                 Console.WriteLine(mes);
-            }
 
-            List<string> lista1 = new List<string>
-            {
-                "Janeiro", "Fevereiro", "Março"
-            };
+            var lista1 = new List<string> { "Janeiro", "Fevereiro", "Março" };
 
-            List<string> lista2 = new List<string>
-            {
-                "Fevereiro", "MARÇO", "Abril"
-            };
+            var lista2 = new List<string> { "Fevereiro", "MARÇO", "Abril" };
 
             Console.WriteLine("\n -= Lista 1 =-");
+
             lista1.ForEach(item => Console.WriteLine(item));
+
             Console.WriteLine("\n -= Lista 2 =-");
+
             lista2.ForEach(item => Console.WriteLine(item));
 
             var consultaLista1 = lista1.Concat(lista2);
+
             Console.WriteLine("\nlista1.Concat(lista2)");
+
             foreach (var item in consultaLista1)
-            {
                 Console.WriteLine(item);
-            }
 
             var consultaLista2 = lista1.Union(lista2);
+
             Console.WriteLine("\nlista1.Union(lista2)");
+
             foreach (var item in consultaLista2)
-            {
                 Console.WriteLine(item);
-            }
 
             var consultaLista3 = lista1.Union(lista2, StringComparer.InvariantCulture);
+
             Console.WriteLine("\nlista1.Union(lista2, StringComparer.InvariantCulture)");
+
             foreach (var item in consultaLista3)
-            {
                 Console.WriteLine(item);
-            }
 
             var consultaLista4 = lista1.Except(lista2);
+
             Console.WriteLine("\nlista1.Except(lista2)");
+
             foreach (var item in consultaLista4)
-            {
                 Console.WriteLine(item);
-            }
 
-            string[] mesesArray = new string[]
-            {
-                "Janeiro",
-                "Fevereiro",
-                "Março",
-                "Abril",
-                "Maio",
-                "Junho",
-                "Julho",
-                "Agosto",
-                "Setembro",
-                "Outubro",
-                "Novembro",
-                "Dezembro"
-            };
-
-            // object[] mesesArrayObj = mesesArray; // ERRADO VAI DAR EXCEPETION AO TENTAR INSERIR VALORES DE OUTRO TIPO QUE NÃO SEJA O TIPO QUE ELE RECEBEU
+            string[] mesesArray = new string[] { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
 
             IEnumerable<object> mesesEnum = meses; // COVARIANÇiA
 
             Console.WriteLine($"\nCOVARIANÇIA E CONTRACOVARIANÇIA");
 
             foreach (var item in mesesEnum)
-            {
                 Console.WriteLine(item);
-            }
 
             Console.Read();
         }
